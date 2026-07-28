ac0ba84bed296b72530c30c26d3a805f53c45c4e
Rahul Raj C P <rahulrajcp@Rahuls-MacBook-Air.local>
2026-07-28 09:45:38 +0530

    object on js

--- ob.js
+++ ob.js
@@ -7,17 +7,65 @@ let products = {
     stock: 80
 }
 
-console.log(products);
+// console.log(products);
 
-// object acces by dot operator
-console.log(products.pname);
+// // object acces by dot operator
+// console.log(products.pname);
 
-// object acces by square bracket
-console.log(products["price"]);
-
-
-// seach the key is in object
-console.log("offers" in products);
+// // object acces by square bracket
+// console.log(products["price"]);
 
 
+// // seach the key is in object
+// console.log("offers" in products);
 
+// search a key not find insert the key and value
+// products.hasOwnProperty("discount")?products
+// ["discount"]:products["discount"]=20%
+
+
+// remove the key value
+// delete products.stock
+// console.log(products);
+
+
+
+let cars={
+    name:"Boleno",
+    model:"hatch-back",
+    manufacture:"maruti suzuki",
+    price:"13 L"
+}
+
+// display car name and its manufacture
+
+console.log(`Car Name: ${cars.name} -  manufacture: ${cars.manufacture}`);
+
+
+// check model key is present then display its value
+
+// console.log("model" in cars?`Model: ${cars.model}`:`No Key Present`);
+cars.hasOwnProperty("model")&&console.log(cars["model"]);
+
+
+
+// add varient as "automatic" and "manual"
+
+cars["varient"]=["automatic","manual"]
+console.log(cars);
+
+// add colors as red , blue , white , ash and black
+
+cars["colors"]=["red","blue","white","ash","black"]
+console.log(cars);
+
+
+// add hybrid varient
+
+cars.varient.push("hybrid")
+console.log(cars);
+
+
+console.log(Object.values(cars));
+
+console.log(Object.keys(cars));
